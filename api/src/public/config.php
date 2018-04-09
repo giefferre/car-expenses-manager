b09dfa1431309a88027935accffba0aa35acde59
Gianfranco Reppucci <gianfranco.reppucci@gmail.com>
2018-04-09 22:00:22 +0200

    add DB & Phinx configuration

--- api/src/public/config.php
+++ api/src/public/config.php
@@ -1,3 +1,7 @@
 <?php
 
 $config['displayErrorDetails'] = true;
+$config['db']['host'] = "127.0.0.1";
+$config['db']['user'] = "root";
+$config['db']['pass'] = "anotherbrickinthewall";
+$config['db']['dbname'] = "cxm_dev";
